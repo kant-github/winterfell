ad244e3e66e852685abe4985db6fa5a9cfb2fb46
Anjansuman <anjansuman80@gmail.com>
2025-12-05 04:56:02 +0530

    generate contract controller update with template use case.

---
+++ packages/database/prisma/migrations/20251204222511_is/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `isTemplate` on the `Contract` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Contract" DROP COLUMN "isTemplate";
